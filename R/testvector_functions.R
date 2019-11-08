c5cdc270b76afb07ba1d63eb72a71d5437e043a0
David Rügamer <David.Ruegamer@cyoss.com>
2019-11-08 08:56:48 +0100

    make conditions more structured, fix sname definition, make grep for name more robust

--- R/testvector_functions.R
+++ R/testvector_functions.R
@@ -26,48 +26,80 @@ testvec_for_gamm4 <- function(mod, name, sigma2 = NULL, nrlocs=7,
   # ind <- grep(name, names)
   
   if("gamm" %in% class(mod)) {
-    sterms <- c(unlist(sapply(mod$gam$smooth,"[[","vn")), names(mod$lme$coefficients$random))
+    sterms <- c(unlist(sapply(mod$gam$smooth,"[[","term")), names(mod$lme$coefficients$random))
   } else {
     sterms <- c(unlist(sapply(mod$gam$smooth,"[[","vn")), names(mod$mer@cnms))
   }
   SigmaInv <- mod$gam$Vp
   if(is.null(sigma2)) sigma2 <- mod$gam$sig2
   
-  if(all(name %in% sterms))
-  {
-    if(complete_effect){
-      # Adapted from iboost package (https://github.com/davidruegamer/iboost/)
-      
-      # from the selectiveInference package
-      svdu_thresh <- function(x) {
-        svdx <- svd(x)
-        inds <- svdx$d > svdx$d[1] * sqrt(.Machine$double.eps)
-        return(svdx$u[, inds, drop = FALSE])
-      }
-      
-      if (length(name) == 1) {
+  if(complete_effect){
+    # Adapted from iboost package (https://github.com/davidruegamer/iboost/)
+    
+    # from the selectiveInference package
+    svdu_thresh <- function(x) {
+      svdx <- svd(x)
+      inds <- svdx$d > svdx$d[1] * sqrt(.Machine$double.eps)
+      return(svdx$u[, inds, drop = FALSE])
+    }
+    
+    if (length(name) == 1) {
       ind_effect <- which(grepl(paste0("s(",name), colnames(modmat), fixed = TRUE))
-      }
-      if (length(name) == 2) {
-        ind_effect <- which(grepl(paste0("te(",paste(name, collapse = ",")), colnames(modmat), fixed = TRUE))
-      }
-      
-      Xj <- modmat[,ind_effect]
-      Xminusj <- svdu_thresh(modmat[,setdiff(1:ncol(modmat),ind_effect)])
-      vTs <- list(t(svdu_thresh(Xj - tcrossprod(Xminusj) %*% Xj)))
-      attr(vTs[[1]], "var") <- sigma2
-      
-    }else{ # single point on a non-linear function
-      
+    }
+    if (length(name) == 2) {
+      ind_effect <- which(grepl(paste0("te(",paste(name, collapse = ",")), colnames(modmat), fixed = TRUE))
+    }
+    
+    Xj <- modmat[,ind_effect]
+    Xminusj <- svdu_thresh(modmat[,setdiff(1:ncol(modmat),ind_effect)])
+    vTs <- list(t(svdu_thresh(Xj - tcrossprod(Xminusj) %*% Xj)))
+    attr(vTs[[1]], "var") <- sigma2
+    
+  }else{ # single point on a (non-)linear function
+    
+    if(all(name %in% sterms))
+    {
       if(length(nrlocs)>1) qs <- nrlocs else qs <-
           quantile(mod$gam$model[[name]], seq(0,1,l=nrlocs+2)[-c(1,nrlocs+2)])
       datap <- as.data.frame(cbind(qs, matrix(0, nrow = length(qs),
                                               ncol = length(pnames) - 1)))
-      names(datap) <- pnames[apply(sapply(pnames, grepl, x = names), 2, any)]
-      lpm <- predict(mod$gam, newdata = datap, type = "lpmatrix")
-      # set columns to zero not associated with the variable
+      this_name <- paste0("s(",name,")")
+
       
-      lpm[, which(!grepl(paste0("s(",name), colnames(lpm), fixed = TRUE))] <- 0
+    }else{
+      
+      datap <- as.data.frame(cbind(1, matrix(0, nrow = 1,
+                                             ncol = length(pnames) - 1)))
+      qs <- 1
+      this_name <- paste0("\\b",name,"\\b")
+
+      
+    }
+    
+    names(datap) <- pnames[apply(sapply(pnames, grepl, x = names), 2, any)]
+    lpm <- predict(mod$gam, newdata = datap, type = "lpmatrix")
+    # set columns to zero not associated with the variable
+    if(all(name %in% sterms)){
+      deactivate <- which(!grepl(this_name, colnames(lpm), fixed=TRUE))
+    }else{
+      deactivate <- which(!grepl(this_name, colnames(lpm)))
+    }
+    lpm[, deactivate] <- 0
+    
+    
+    if("gamm" %in% class(mod)){
+      
+      hm_obj <- hatmatfun_gamm(mod)
+      vTs <- lapply(1:nrow(lpm), function(j){
+        k <- lpm[j,]%*%hm_obj[[2]]
+        attr(k, "var") <- as.numeric(lpm[j,]%*%hm_obj[[1]]%*%lpm[j,])*sigma2/mod$gam$sig2
+        attr(k, "loc") <- qs[j]
+        return(k)
+      })
+      
+    }else{
+      
+      warning("gamm4 inference currently not well tested.")
       vTs <- lapply(1:nrow(lpm), function(j){
         k <- lpm[j,]%*%SigmaInv%*%t(modmat)/mod$gam$sig2
         attr(k, "var") <- as.numeric(lpm[j,]%*%SigmaInv%*%lpm[j,])*sigma2/mod$gam$sig2
@@ -77,66 +109,10 @@ testvec_for_gamm4 <- function(mod, name, sigma2 = NULL, nrlocs=7,
       
     }
     
-    #
-    # return(lapply(vTs, t))
-    
-  }else{
-    
-    # # selind <- which(colnames(modmat)==name)
-    # # find name in colnames of model matrix
-    # selind <- grepl(pattern = paste0(name, "[:digit:]*"), colnames(modmat)) &
-    #   # remove interactions
-    #   !grepl(pattern = ":", colnames(modmat), fixed=TRUE)
-    # # vTs <- list(((SigmaInv%*%t(modmat))/sigma2)[selind, , drop=FALSE])
-    # vTs <- testvec_for_mm(mod = mod$mer, which = which(selind),
-    #                       marginal = FALSE, sig2 = sigma2, efficient = TRUE)
-    # attr(vTs[[1]], "var") <- (SigmaInv)[selind,selind]*sigma2/mod$gam$sig2
-    
-    if(length(nrlocs)>1) qs <- nrlocs else qs <-
-        quantile(mod$gam$model[[name]], seq(0,1,l=nrlocs+2)[-c(1,nrlocs+2)])
-    datap <- as.data.frame(cbind(1, matrix(0, nrow = 1,
-                                            ncol = length(pnames) - 1)))
-    names(datap) <- pnames[apply(sapply(pnames, grepl, x = names), 2, any)]
-    lpm <- predict(mod$gam, newdata = datap, type = "lpmatrix")
-    # set columns to zero not associated with the variable
-    this_name <- paste0("\\b",name,"\\b")
-    lpm[, which(!grepl(this_name, colnames(lpm)))] <- 0
-    
-    if("gamm" %in% class(mod)){
-      
-      hm_obj <- hatmatfun_gamm(mod)
-      vTs <- lapply(1:nrow(lpm), function(j){
-        k <- lpm[j,]%*%hm_obj[[2]]
-        attr(k, "var") <- as.numeric(lpm[j,]%*%hm_obj[[1]]%*%lpm[j,])*sigma2/mod$gam$sig2
-        # attr(k, "loc") <- qs[j]
-        return(k)
-      })
-      
-    }else{
-    
-      warning("gamm4 inference currently not well tested.")
-      vTs <- lapply(1:nrow(lpm), function(j){
-        k <- lpm[j,]%*%SigmaInv%*%t(modmat)/mod$gam$sig2
-        attr(k, "var") <- as.numeric(lpm[j,]%*%SigmaInv%*%lpm[j,])*sigma2/mod$gam$sig2
-        # attr(k, "loc") <- qs[j]
-        return(k)
-      })
-    
-    }
     
   }
   
   return(vTs)
-  
-  # if(plot)
-  # {
-  #
-  #   plot(mod$gam, select = which(name%in%attr(mod$gam$terms,"term.labels")), ylim = c(-7,9))
-  #   intcpt <- coef(mod$gam)[1]
-  #   points(qs, sapply(vTs, function(vT) vT%*%this_y) - intcpt)
-  #   points(qs, truefun(qs) - intcpt,  col="red")
-  #
-  # }
   
 }
 
